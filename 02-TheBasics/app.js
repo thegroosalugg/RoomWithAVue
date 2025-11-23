7e25d624fbff01c4810e7b4798baba0e3e047996
victor loginov <tkoe88@zoho.com>
2025-11-23 22:59:58 +0100

    lesson: event modifiers

--- 02-TheBasics/app.js
+++ 02-TheBasics/app.js
@@ -1,21 +1,35 @@
 const app = Vue.createApp({
   data() {
     return {
-      someText: "This static text will change on mounted() timeout",
-      someLink: "http://localhost:5500/",
-      counter: 0,
+       someText: "This static text will change on mounted() timeout",
+       someLink: "http://localhost:5500/",
+      inputText: "This will update on change",
+      keyDnText: "This will update on CTRL key down",
+        counter: 0,
+         status: "Click with Left/Middle/Right mouse"
     };
   },
   // Vue lifecycle hook that runs when DOM is ready to execute side effects
   mounted() {
     setTimeout(() => {
-      this.someText = "Timeout executed, App has loaded"
+      this.someText = "Timeout executed, App has mounted"
     }, 5000);
   },
   methods: {
     randomNum() {
       return Math.random().toString().slice(2, 9).split('').join(', ');
     },
+    changeText(event, arg) {
+      const value = event.target.value?.trim() || `This will update on ${arg || 'CTRL key down'}`;
+      if (arg) this.inputText = value;
+      else     this.keyDnText = value;
+    },
+    onClick(side) {
+      this.status = `${side} Click`;
+    },
+    onSubmit() {
+      alert("Submitted");
+    }
   },
 });
 
