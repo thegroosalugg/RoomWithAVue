6bdc8f69cf5c7dfb241cfb31804a39b25f7376e9
victor loginov <tkoe88@zoho.com>
2025-11-24 17:37:56 +0100

    lesson: watchers & computed

--- 02-TheBasics/app.js
+++ 02-TheBasics/app.js
@@ -16,6 +16,19 @@ const app = Vue.createApp({
       this.someText = "Timeout executed, App has mounted"
     }, 5000);
   },
+  // watchers watch a value inside data() and execute some function when it changes. Not used directly in template
+  watch: {
+    counter() {
+      if (this.counter > 10 || this.counter < -10) this.counter = 0;
+    }
+  },
+  // computed like in Angular - used with data binding
+  computed: {
+    computedNum() {
+      return this.randomNum();
+    }
+  },
+  // used with event binding (or data binding for data that needs to be re-evaluated on every re-render)
   methods: {
     randomNum() {
       return Math.random().toString().slice(2, 9).split('').join(', ');
