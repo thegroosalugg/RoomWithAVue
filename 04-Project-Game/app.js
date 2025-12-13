61f981de30d26bf8b0c7cb907fc3e7c8b3433c22
victor loginov <tkoe88@zoho.com>
2025-12-13 03:17:43 +0100

    created attack function

--- 04-Project-Game/app.js
+++ 04-Project-Game/app.js
@@ -2,13 +2,51 @@ function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const data = {
-   you: 100,
-  them: 100,
+function getHP(hp) {
+  const width = Math.max(hp, 0) + '%';
+  const backgroundColor = `var(--${hp < 25 ? 'danger' : hp < 50 ? 'warning' : 'success'})`;
+  return { width, backgroundColor };
+}
+
+function data() {
+  return {
+     you: 100,
+    them: 100,
+    isProcessing: false,
+  };
 };
 
-const methods = {};
+const computed = {
+  yourHealth() {
+    return getHP(this.you);
+  },
+  theirHealth() {
+    return getHP(this.them);
+  },
+  isDisabled() {
+    return this.isProcessing || this.you < 0 || this.them < 0;
+  }
+};
 
-const app = Vue.createApp({ data, methods });
+const methods = {
+  process(callback) {
+    if (this.isProcessing) return;
+    this.isProcessing = true;
+    callback();
+    setTimeout(() => {
+      this.isProcessing = false;
+    }, 1000);
+  },
+  attack() {
+    this.process(() => {
+      this.them -= rand(2, 9);
+      setTimeout(() => {
+        this.you -= rand(5, 15);
+      }, 500);
+    });
+  }
+};
+
+const app = Vue.createApp({ data, computed, methods });
 
 app.mount("#game");
